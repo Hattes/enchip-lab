831b4472db7dc925f205ea017777ca4d9a996ea4
Hannes Sörensson <hannes.sorensson@gmail.com>
2024-07-14 17:55:39 +0200

    lab2: working after some help

--- lab2-trafikljus/Core/Src/main.c
+++ lab2-trafikljus/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -26,6 +26,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+
 typedef enum
 {
 	s_init,
@@ -35,17 +36,21 @@ typedef enum
 	s_cars_stop,
 	s_pedestrians_go,
 	s_cars_start,
+	s_NUM_STATES,
 } TrafficLightState;
+
 typedef enum
 {
 	ev_none,
 	ev_button_press,
 	ev_state_timeout
-} event;
+} TrafficEvent;
+
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define USING_ISR 1
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -58,9 +63,9 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 TrafficLightState state;
-event last_event;
+TrafficEvent last_event;
 int ticks_left_in_state;
-int button_pressed_prev_tick = 0;
+int button_pressed_prev_tick = 0; // true/false
 uint32_t before;
 /* USER CODE END PV */
 
@@ -72,7 +77,7 @@ static void MX_USART2_UART_Init(void);
 int is_blue_button_pressed();
 void set_traffic_lights(TrafficLightState tls);
 void tick();
-event get_event(int check_button);
+TrafficEvent get_event(int check_button);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -92,7 +97,7 @@ void set_traffic_lights(TrafficLightState tls)
 	switch (tls)
 	{
 	case s_init:
-		cr = pr = GPIO_PIN_SET;
+		cr = cy = cg = pr = pg = GPIO_PIN_SET;
 		break;
 	case s_all_stop:
 		cr = pr = GPIO_PIN_SET;
@@ -111,11 +116,11 @@ void set_traffic_lights(TrafficLightState tls)
 		cy = pr = GPIO_PIN_SET;
 		break;
 	}
-	HAL_GPIO_WritePin(GPIOC, CAR_RED_Pin, cr);
-	HAL_GPIO_WritePin(GPIOC, CAR_YLW_Pin, cy);
-	HAL_GPIO_WritePin(GPIOC, CAR_GRN_Pin, cg);
-	HAL_GPIO_WritePin(GPIOC, PED_RED_Pin, pr);
-	HAL_GPIO_WritePin(GPIOC, PED_GRN_Pin, pg);
+	HAL_GPIO_WritePin(CAR_RED_GPIO_Port, CAR_RED_Pin, cr);
+	HAL_GPIO_WritePin(CAR_YLW_GPIO_Port, CAR_YLW_Pin, cy);
+	HAL_GPIO_WritePin(CAR_GRN_GPIO_Port, CAR_GRN_Pin, cg);
+	HAL_GPIO_WritePin(PED_RED_GPIO_Port, PED_RED_Pin, pr);
+	HAL_GPIO_WritePin(PED_GRN_GPIO_Port, PED_GRN_Pin, pg);
 	return;
 }
 
@@ -126,7 +131,7 @@ void tick() {
 	return;
 }
 
-event get_event(int check_button) {
+TrafficEvent get_event(int check_button) {
 	if (check_button) {
 		if (is_blue_button_pressed()) {
 			if (!button_pressed_prev_tick) {
@@ -144,262 +149,291 @@ event get_event(int check_button) {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* USER CODE END 1 */
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* USER CODE BEGIN Init */
-  before = HAL_GetTick();
-  /* USER CODE END Init */
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* USER CODE BEGIN Init */
+	before = HAL_GetTick();
+	/* USER CODE END Init */
 
-  /* USER CODE BEGIN SysInit */
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE END SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  /* USER CODE BEGIN 2 */
-  state = s_init;
-  set_traffic_lights(state);
-  char str[81] = { '\0'};
-  uint16_t str_len = 0;
-  str_len = sprintf(str, "Test\r\n");
-  HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
-  /* USER CODE END 2 */
+	/* USER CODE END SysInit */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	/* USER CODE BEGIN 2 */
+	state = s_init;
+	set_traffic_lights(state);
+	//char str[81] = { '\0'};
+	//uint16_t str_len = 0;
+	//str_len = sprintf(str, "Test\r\n");
+//	HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
+	/* USER CODE END 2 */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+
+	TrafficLightState state = s_init;
+	TrafficEvent ev = ev_none;
+	int curr_pressed, last_pressed;
+	curr_pressed = last_pressed = is_blue_button_pressed();
+	uint32_t curr_tick, last_tick;
+	curr_tick = last_tick = HAL_GetTick();
+	int32_t ticks_left_in_state = 0;
+
+	set_traffic_lights(state);
 	while (1) {
-		tick();
-		event new_event = get_event(state == s_cars_go);
-		if (new_event == ev_none) {
-			//str_len = sprintf(str, "Got none\r\n");
-			//HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
-		}
-		if (new_event == ev_state_timeout) {
-			str_len = sprintf(str, "Got timeout\r\n");
-			HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
-		}
-		if (new_event == ev_button_press) {
-			str_len = sprintf(str, "Got button press\r\n");
-			HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
-		}
-		//str_len = sprintf(str, "Got event %d\r\n", new_event);
-		//HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
 
-		//str_len = sprintf(str, "Test2\r\n");
-		//HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
+#ifdef USING_ISR
+#else
+
+		last_pressed = curr_pressed;
+		curr_pressed = is_blue_button_pressed();
+
+
+		if (curr_pressed && !last_pressed)
+		{
+			ev = ev_button_press;
+		}
+		else if (ticks_left_in_state > 0)
+		{
+			last_tick = curr_tick;
+			curr_tick = HAL_GetTick();
+			uint32_t delta = curr_tick - last_tick;
+			if (delta != 0)
+			{
+				ticks_left_in_state -= 1;
+				if (ticks_left_in_state == 0)
+				{
+					ev = ev_state_timeout;
+				}
+				else
+				{
+					ev = ev_none;
+				}
+			}
+		}
+		else
+		{
+			ev = ev_none;
+		}
+#endif
 
 		switch(state)
 		{
 		case s_init:
-			state = s_all_stop;
-			ticks_left_in_state = 2000;
-			new_event 			= ev_none;
-			str_len = sprintf(str, "Moving from s_init to s_all_stop\r\n");
-			HAL_UART_Transmit(&huart2, (uint8_t*) str, str_len, HAL_MAX_DELAY);
+			if (ev == ev_button_press) {
+				state = s_all_stop;
+				ticks_left_in_state = 2000;
+				ev = ev_none;
+				set_traffic_lights(s_all_stop);
+			}
+			break;
 		case s_all_stop:
-			if (new_event == ev_state_timeout) {
+			if (ev == ev_state_timeout) {
 				state 				= s_cars_go;
 				ticks_left_in_state = 0;
-				new_event 			= ev_none;
+				ev 			= ev_none;
 				set_traffic_lights(s_cars_go);
 			}
 			break;
 		case s_cars_go:
-			if (new_event == ev_button_press) {
+			if (ev == ev_button_press) {
 				state 				= s_button_pressed;
 				ticks_left_in_state = 3000;
-				new_event 			= ev_none;
+				ev 			= ev_none;
 				set_traffic_lights(s_button_pressed);
 			}
 			break;
 		case s_button_pressed:
-			if (new_event == ev_state_timeout) {
+			if (ev == ev_state_timeout) {
 				state 				= s_cars_stop;
 				ticks_left_in_state = 2500;
-				new_event 			= ev_none;
+				ev 			= ev_none;
 				set_traffic_lights(s_cars_stop);
 			}
 			break;
 		case s_cars_stop:
-			if (new_event == ev_state_timeout) {
+			if (ev == ev_state_timeout) {
 				state				= s_pedestrians_go;
 				ticks_left_in_state = 5000;
-				new_event			= ev_none;
+				ev			= ev_none;
 				set_traffic_lights(s_pedestrians_go);
 			}
 			break;
 		case s_pedestrians_go:
-			if (new_event == ev_state_timeout) {
+			if (ev == ev_state_timeout) {
 				state 				= s_cars_start;
 				ticks_left_in_state = 2500;
-				new_event 			= ev_none;
+				ev 			= ev_none;
 				set_traffic_lights(s_cars_start);
 			}
 			break;
 		case s_cars_start:
-			if (new_event == ev_state_timeout) {
+			if (ev == ev_state_timeout) {
 				state 				= s_cars_go;
 				ticks_left_in_state = 0;
-				new_event 			= ev_none;
+				ev 			= ev_none;
 				set_traffic_lights(s_cars_go);
 			}
 			break;
 		}
 	}
-		//for (TrafficLightState s = s_init; s <= s_cars_start; s++) {
-		//set_traffic_lights(s);
-		//HAL_Delay(300);
-		//}
-    /* USER CODE END WHILE */
+	//for (TrafficLightState s = s_init; s <= s_cars_start; s++) {
+	//set_traffic_lights(s);
+	//HAL_Delay(300);
+	//}
+	/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+	/* USER CODE BEGIN 3 */
 }
+/* USER CODE END 3 */
+
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 16;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+	RCC_OscInitStruct.PLL.PLLQ = 7;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART2_Init 0 */
+	/* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END USART2_Init 0 */
+	/* USER CODE END USART2_Init 0 */
 
-  /* USER CODE BEGIN USART2_Init 1 */
+	/* USER CODE BEGIN USART2_Init 1 */
 
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
+	/* USER CODE END USART2_Init 1 */
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+	if (HAL_UART_Init(&huart2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/* USER CODE BEGIN USART2_Init 2 */
 
-  /* USER CODE END USART2_Init 2 */
+	/* USER CODE END USART2_Init 2 */
 
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
+	/* USER CODE END MX_GPIO_Init_1 */
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, CAR_RED_Pin|CAR_YLW_Pin|CAR_GRN_Pin|PED_RED_Pin
-                          |PED_GRN_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOC, CAR_RED_Pin|CAR_YLW_Pin|CAR_GRN_Pin|PED_RED_Pin
+			|PED_GRN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : B1_Pin */
+	GPIO_InitStruct.Pin = B1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : CAR_RED_Pin CAR_YLW_Pin CAR_GRN_Pin PED_RED_Pin
+	/*Configure GPIO pins : CAR_RED_Pin CAR_YLW_Pin CAR_GRN_Pin PED_RED_Pin
                            PED_GRN_Pin */
-  GPIO_InitStruct.Pin = CAR_RED_Pin|CAR_YLW_Pin|CAR_GRN_Pin|PED_RED_Pin
-                          |PED_GRN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	GPIO_InitStruct.Pin = CAR_RED_Pin|CAR_YLW_Pin|CAR_GRN_Pin|PED_RED_Pin
+			|PED_GRN_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : LD2_Pin */
+	GPIO_InitStruct.Pin = LD2_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
+	/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -407,33 +441,33 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
